d2d8230aa3ea89e85fd94f239bcf32eb07d8ed97
Vova.Matsak <vmats@softserveinc.com>
2022-01-17 12:16:33 +0200

    Second commit of HW

---
+++ src/main/java/com/softserveinc/task03/Country.java
@@ -0,0 +1,92 @@
+package com.softserveinc.task03;
+
+import com.softserveinc.task02.City;
+import java.util.Arrays;
+
+public class Country {
+    private String name;
+    private String continent;
+    private String code;
+    private City capital;
+    private City[] cities;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContinent() {
+        return continent;
+    }
+
+    public void setContinent(String continent) {
+        this.continent = continent;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public City getCapital() {
+        return capital;
+    }
+
+    public void setCapital(City capital) {
+        this.capital = capital;
+    }
+
+    public City[] getCities() {
+        return cities;
+    }
+
+    public void setCities(City[] cities) {
+        this.cities = cities;
+    }
+
+    public Country(String name, String continent, String code, City capital) {
+        this.name = name;
+        this.continent = continent;
+        this.code = code;
+        this.capital = capital;
+    }
+
+    public Country() {
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "name='" + name + '\'' +
+                ", continent='" + continent + '\'' +
+                ", code='" + code + '\'' +
+                ", capital=" + capital +
+                ", cities=" + Arrays.toString(cities) +
+                '}';
+    }
+
+    public void addCity(City city) {
+        cities = Arrays.copyOf(cities, cities.length + 1);
+        cities[cities.length - 1] = city;
+    }
+
+    public int getInhabitants() {
+        int inhabitants = 0;
+
+        if (cities.length == 0) {
+            return 0;
+        }
+
+        for (int i = 0; i <= cities.length; i++) {
+            inhabitants += cities[i].getInhabitants();
+        }
+
+        return inhabitants;
+    }
+}
